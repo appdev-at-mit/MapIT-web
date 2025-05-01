a0560af2d8554f60a35dfca3b311112b8df7b9b9
Nithid Anchaleenukoon <nithidan@mit.edu>
2025-05-01 19:05:08 -0400

    Put fireroad api in environment

--- server/api.js
+++ server/api.js
@@ -279,7 +279,8 @@ router.get("/class", (req, res) => {
       res.status(404).send({ msg: 'Class not found'});
   } 
 
-  fetch(`https://fireroad-dev.mit.edu/courses/lookup/${searchText}?full=true`)
+  const baseUrl = process.env.FIREROAD_API;
+  fetch(`https://${baseUrl}/courses/lookup/${searchText}?full=true`)
   .then((response) => {
       if (response.ok) { return response.json(); }
       else {throw new Error(`cannot retrieve class: response ${response.status}`)}
